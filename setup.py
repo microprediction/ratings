b023f2429a17af9ff70b575a43571e53dbcf716d
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-06-30 11:12:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="ratings",
-    version="0.2.8",
+    version="0.2.9",
     description="contests",
     long_description=README,
     long_description_content_type="text/markdown",
